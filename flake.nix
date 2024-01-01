c1a661c25220f7f30b16600754f06b7127f5cc23
fanshi1028 <jackychany321@gmail.com>
2024-01-01 18:13:51 +0800

    refactor nix and update README with nix build instruction

--- flake.nix
+++ flake.nix
@@ -6,8 +6,17 @@
       mkHsPackage = pkgs: pkgs.haskell.packages."ghc${ghcVersion}";
     in {
       packages = builtins.mapAttrs (system: pkgs: {
-        default = ((mkHsPackage pkgs).developPackage {
-          root = ./.;
+        youtube-mpv-native-host = ((mkHsPackage pkgs).developPackage {
+          root = with pkgs.lib.fileset;
+            toSource {
+              root = ./.;
+              fileset = unions [
+                ./youtube-mpv-native-host.cabal
+                ./native-host
+                ./LICENSE
+                ./CHANGELOG.md
+              ];
+            };
           modifier = drv: pkgs.haskell.lib.appendConfigureFlag drv "-O2";
         }).overrideAttrs (_: oa: {
           # NOTE: https://stackoverflow.com/a/69395418
@@ -19,13 +28,54 @@
             }
           '';
         });
+        youtube-mpv-chrome-extension = with pkgs;
+          buildNpmPackage {
+            pname = "youtube-mpv-chrome-extension";
+            version = "1.0.0";
+            nativeBuildInputs = [ typescript ];
+            src = with lib.fileset;
+              toSource {
+                root = ./.;
+                fileset = unions [
+                  ./extension/src
+                  ./package.json
+                  ./package-lock.json
+                  ./tsconfig.json
+                  (fileFilter (file: !(file.hasExt "js")) ./extension/dist)
+                ];
+              };
+            npmDepsHash = "sha256-Z1IhQt2Yul1AjX3V1F0VHvjCOAxvDkxfOf3iNVfdI4E=";
+            buildPhase = "tsc ";
+            postInstall = ''
+              mkdir -p $out/share/chrome/extensions
+              cp -r extension/dist/* $out/share/chrome/extensions
+              rm -r $out/lib
+            '';
+          };
+        youtube-mpv = with pkgs;
+          let
+            inherit (self.packages.${system})
+              youtube-mpv-native-host youtube-mpv-chrome-extension;
+          in stdenvNoCC.mkDerivation {
+            pname = "youtube-mpv";
+            version = "1.0.0";
+            buildInputs =
+              [ youtube-mpv-native-host youtube-mpv-chrome-extension ];
+            phases = [ "installPhase" ];
+            installPhase = ''
+              mkdir $out
+              cp -r ${youtube-mpv-native-host}/bin $out
+              cp -r ${youtube-mpv-chrome-extension}/share $out
+            '';
+          };
+        default = self.packages.${system}.youtube-mpv;
       }) nixpkgs.legacyPackages;
       devShells = builtins.mapAttrs (system: pkgs:
         with pkgs;
         let hsPackage = mkHsPackage pkgs;
         in {
           default = (mkHsPackage pkgs).shellFor {
-            packages = _: [ self.packages.${system}.default ];
+            packages = _: [ self.packages.${system}.youtube-mpv-native-host ];
             nativeBuildInputs = with pkgs;
               [
                 (haskell-language-server.override {
@@ -39,6 +89,7 @@
                 typescript-language-server
                 pnpm
                 prettier
+                npm
               ]);
             withHoogle = true;
           };
