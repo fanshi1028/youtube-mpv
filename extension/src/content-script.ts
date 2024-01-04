56b15d992de0ada8dd39511d43a20dc57b3899c9
fanshi1028 <jackychany321@gmail.com>
2024-01-04 16:15:51 +0800

    fix the overrided UI and support youtube.com/serach page

--- extension/src/content-script.ts
+++ extension/src/content-script.ts
@@ -6,7 +6,8 @@ const observer = new MutationObserver((mutationList, _observer) => {
         if (
           node.nodeType == 1 &&
           node instanceof Element &&
-          node.tagName == "YTD-RICH-GRID-MEDIA"
+          (node.tagName == "YTD-RICH-GRID-MEDIA" ||
+            node.tagName == "YTD-VIDEO-RENDERER")
         ) {
           const dismissible = node.firstElementChild;
           const thumbnail = dismissible?.firstElementChild;
@@ -16,20 +17,29 @@ const observer = new MutationObserver((mutationList, _observer) => {
               () => {
                 const a_thumbnail = thumbnail.querySelector("a#thumbnail");
 
-                const match = /^\/watch\?v=(.+)$/g.exec(
-                  a_thumbnail?.getAttribute("href") || "",
-                );
-                const v = match && match[1].split("&")[0];
-                const img = a_thumbnail?.querySelector("img");
-
-                if (img && v) {
-                  img.addEventListener("click", () =>
-                    chrome.runtime.sendMessage({ v }, (res) =>
-                      console.log(res),
-                    ),
+                if (a_thumbnail) {
+                  const match = /^\/watch\?v=(.+)$/g.exec(
+                    a_thumbnail.getAttribute("href") || "",
                   );
-                  dismissible.replaceChild(img, thumbnail);
-                } else console.log(`fuck, img: ${img}, v: ${v}`);
+                  const v = match && match[1].split("&")[0];
+                  const img = a_thumbnail.querySelector("img");
+
+                  if (img && v) {
+                    img.classList.add("ytd-thumbnail");
+                    img.style.borderRadius = "12px";
+                    if (node.tagName == "YTD-VIDEO-RENDERER") {
+                      img.style.marginRight = "16px";
+                      img.style.maxWidth = "360px";
+                      img.style.minWidth = "240px";
+                    }
+                    img.addEventListener("click", () =>
+                      chrome.runtime.sendMessage({ v }, (res) =>
+                        console.log(res),
+                      ),
+                    );
+                    dismissible.replaceChild(img, thumbnail);
+                  } else console.log(`fuck, img: ${img}, v: ${v}`);
+                } else console.log(`fuck, a_thumbnail: ${a_thumbnail}`);
               },
               { once: true },
             );
