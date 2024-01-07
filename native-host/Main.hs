dce913d333e6ee9aa8a629721efafe39b8f6ba8a
fanshi1028 <jackychany321@gmail.com>
2024-01-07 21:40:51 +0800

    no log file, send logs to extension console instead & fix bugs

--- native-host/Main.hs
+++ native-host/Main.hs
@@ -5,13 +5,12 @@ module Main where
 
 import Control.Concurrent.Async (race)
 import Control.Exception (SomeException, bracket, try)
-import Data.Aeson as JSON (FromJSON (parseJSON), KeyValue ((.=)), ToJSON (toEncoding, toJSON), encode, pairs)
-import Data.Aeson.Encoding (encodingToLazyByteString)
-import Data.Binary as B (Binary (get, put), encode)
+import Data.Aeson as JSON (KeyValue ((.=)), ToJSON (toEncoding, toJSON), encode, pairs)
+import Data.Binary as B (Binary (get, put))
 import Data.Binary.Get (getLazyByteString, getWord32host, runGetOrFail)
 import Data.Binary.Put (execPut, putLazyByteString, putWord32host)
 import Data.ByteString.Builder (hPutBuilder)
-import Data.ByteString.Lazy qualified as BL (ByteString, getContents, length, putStr)
+import Data.ByteString.Lazy qualified as BL (ByteString, getContents, length)
 import Data.ByteString.Lazy.Char8 as BL (lines)
 import Data.Foldable (traverse_)
 import Data.Text as T (Text, pack)
@@ -19,24 +18,11 @@ import Network.Socket (Family (AF_UNIX), SocketType (Datagram), close, defaultPr
 import Network.Socket.ByteString.Lazy as SOC (getContents, sendAll)
 import System.Directory.OsPath (createDirectoryIfMissing, getHomeDirectory)
 import System.Exit (ExitCode (ExitSuccess))
-import System.IO (BufferMode (BlockBuffering, LineBuffering), Handle, hClose, hFlush, hPutStrLn, hSetBinaryMode, hSetBuffering, openTempFile, stdin, stdout)
+import System.IO (BufferMode (BlockBuffering), Handle, hClose, hFlush, hSetBinaryMode, hSetBuffering, openTempFile, stdin, stdout)
 import System.OsPath as FP (decodeUtf, encodeUtf, (</>))
-import System.Process (CreateProcess (std_err, std_in, std_out), StdStream (NoStream), interruptProcessGroupOf, shell, waitForProcess, withCreateProcess)
+import System.Process (CreateProcess (std_err, std_in, std_out), StdStream (NoStream), shell, waitForProcess, withCreateProcess)
 
 newtype NativeMessage a = NativeMessage a deriving (Show, Eq, ToJSON)
-
-newtype MPVError = MPVError Text deriving (Show, Eq)
-
-instance FromJSON MPVError where
-  parseJSON = undefined
-
-instance ToJSON MPVError where
-  toJSON = undefined
-  toEncoding (MPVError err) = pairs $ "error" .= err
-
-instance Binary (NativeMessage MPVError) where
-  put (NativeMessage err) = put $ JSON.encode err
-  get = undefined
 
 instance Binary (NativeMessage BL.ByteString) where
   put (NativeMessage bs) = do
@@ -46,8 +32,24 @@ instance Binary (NativeMessage BL.ByteString) where
     NativeMessage <$> do
       getWord32host >>= getLazyByteString . fromIntegral
 
+instance {-# OVERLAPPABLE #-} (ToJSON a) => Binary (NativeMessage a) where
+  put (NativeMessage err) = put . NativeMessage $ JSON.encode err
+  get = undefined
+
 hEncodeAndSend :: (Binary (NativeMessage a)) => a -> IO ()
 hEncodeAndSend a = hPutBuilder stdout . execPut . put $ NativeMessage a
+
+newtype MPVError = MPVError Text deriving (Show, Eq)
+
+instance ToJSON MPVError where
+  toJSON = undefined
+  toEncoding (MPVError err) = pairs $ "error" .= err
+
+newtype MPVInfo = MPVInfo Text deriving (Show, Eq)
+
+instance ToJSON MPVInfo where
+  toJSON = undefined
+  toEncoding (MPVInfo info) = pairs $ "info" .= info
 
 withMpvTempFile :: String -> ((FilePath, Handle) -> IO b) -> IO b
 withMpvTempFile name io = do
@@ -65,36 +67,28 @@ main = do
   hSetBinaryMode stdin True
   hSetBuffering stdout $ BlockBuffering Nothing
   hSetBinaryMode stdout True
-  withMpvTempFile "log" $ \(_logFile, hLog) -> do
-    hSetBuffering hLog LineBuffering
-    let printError err = hPutStrLn hLog $ "error: " <> err
-        printInfo info = hPutStrLn hLog $ "info: " <> info
-    bracket (socketPair AF_UNIX Datagram defaultProtocol) (\(soc1, soc2) -> close soc1 *> close soc2) $ \(soc1, soc2) -> do
-      let runMpvIPC = withFdSocket soc2 $ \fd -> do
-            let process = (shell $ "mpv --input-ipc-client=fd://" <> show fd <> " --idle --keep-open") {std_in = NoStream, std_out = NoStream, std_err = NoStream}
-            withCreateProcess process $ \mHIn mHOut mHErr h -> case (mHIn, mHOut, mHErr) of
-              (Just _, _, _) -> pure . Left $ MPVError "impossible: std_in handle exists for mpv"
-              (_, Just _, _) -> pure . Left $ MPVError "impossible: std_out handle exists for mpv"
-              (_, _, Just _) -> pure . Left $ MPVError "impossible: std_err handle exists for mpv"
-              (Nothing, Nothing, Nothing) ->
-                let parseAndSendCommand l = case runGetOrFail get l of
-                      Right (uncomsumed, b, NativeMessage cmd) -> do
-                        printInfo $ "Comsumed " <> show b <> " bytes."
-                        printInfo $ show cmd
-                        sendAll soc1 cmd
-                        sendAll soc1 "\n"
-                        parseAndSendCommand uncomsumed
-                      Left _ -> pure ()
-                    commandLoop = BL.getContents >>= parseAndSendCommand
-                 in (MPVError "commadLoop ended before mpv" <$ commandLoop) `race` waitForProcess h
-          mpvOutputPrintloop =
-            SOC.getContents soc1
-              >>= traverse_ ((*> hFlush stdout) . BL.putStr . B.encode . NativeMessage) . BL.lines
-      try ((MPVError "mpvOutputPrintloop ended before mpv" <$ mpvOutputPrintloop) `race` runMpvIPC) >>= \case
-        Left (err :: SomeException) -> do
-          printError $ show err
-          hEncodeAndSend $ MPVError . T.pack $ show err
-        Right (Right (Right ExitSuccess)) -> printInfo "Done, all good"
-        Right (Right (Right oops)) -> hEncodeAndSend . MPVError . T.pack $ show oops
-        Right (Right (Left oops)) -> hEncodeAndSend oops
-        Right (Left err) -> hEncodeAndSend err
+  bracket (socketPair AF_UNIX Datagram defaultProtocol) (\(soc1, soc2) -> close soc1 *> close soc2) $ \(soc1, soc2) -> do
+    let runMpvIPC = withFdSocket soc2 $ \fd -> do
+          let process = (shell $ "mpv --input-ipc-client=fd://" <> show fd <> " --idle --keep-open") {std_in = NoStream, std_out = NoStream, std_err = NoStream}
+          withCreateProcess process $ \mHIn mHOut mHErr h -> case (mHIn, mHOut, mHErr) of
+            (Just _, _, _) -> pure . Left $ MPVError "impossible: std_in handle exists for mpv"
+            (_, Just _, _) -> pure . Left $ MPVError "impossible: std_out handle exists for mpv"
+            (_, _, Just _) -> pure . Left $ MPVError "impossible: std_err handle exists for mpv"
+            (Nothing, Nothing, Nothing) ->
+              let parseAndSendCommand l = case runGetOrFail get l of
+                    Right (uncomsumed, b, NativeMessage cmd) -> do
+                      hEncodeAndSend . MPVInfo $ "Comsumed " <> T.pack (show b) <> " bytes."
+                      hEncodeAndSend cmd
+                      sendAll soc1 cmd
+                      sendAll soc1 "\n"
+                      parseAndSendCommand uncomsumed
+                    Left _ -> pure ()
+                  commandLoop = BL.getContents >>= parseAndSendCommand
+               in (MPVError "commadLoop ended before mpv" <$ commandLoop) `race` waitForProcess h
+        mpvOutputPrintloop = SOC.getContents soc1 >>= traverse_ ((*> hFlush stdout) . hEncodeAndSend) . BL.lines
+    try ((MPVError "mpvOutputPrintloop ended before mpv" <$ mpvOutputPrintloop) `race` runMpvIPC) >>= \case
+      Left (err :: SomeException) -> hEncodeAndSend . MPVError . T.pack $ show err
+      Right (Right (Right ExitSuccess)) -> hEncodeAndSend $ MPVInfo "Done, all good"
+      Right (Right (Right oops)) -> hEncodeAndSend . MPVError . T.pack $ show oops
+      Right (Right (Left oops)) -> hEncodeAndSend oops
+      Right (Left err) -> hEncodeAndSend err
