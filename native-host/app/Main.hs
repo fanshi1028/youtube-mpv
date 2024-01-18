0992a080089e0369c860fa114d0e15c0ecdf66c3
fanshi1028 <jackychany321@gmail.com>
2024-01-18 18:21:13 +0800

    refactor for a bit

--- native-host/app/Main.hs
+++ native-host/app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
@@ -16,9 +17,12 @@ import Data.Foldable (traverse_)
 import Data.Text as T (Text, pack)
 import Network.Socket (Family (AF_UNIX), SocketType (Datagram), close, defaultProtocol, socketPair, withFdSocket)
 import Network.Socket.ByteString.Lazy as SOC (getContents, sendAll)
-import System.Exit (ExitCode (ExitSuccess))
+import System.Directory.OsPath (findExecutable)
+import System.Exit (ExitCode (ExitFailure, ExitSuccess), exitWith)
 import System.IO (BufferMode (BlockBuffering), hFlush, hSetBinaryMode, hSetBuffering, stdin, stdout)
-import System.Process.Typed (nullStream, setStderr, setStdin, setStdout, shell, waitExitCode, withProcessWait)
+import System.OsPath qualified as FP (decodeUtf)
+import System.OsString (osstr)
+import System.Process.Typed (nullStream, proc, setStderr, setStdin, setStdout, waitExitCode, withProcessTerm)
 
 newtype NativeMessage a = NativeMessage a deriving (Show, Eq, ToJSON)
 
@@ -55,26 +59,38 @@ main = do
   hSetBinaryMode stdin True
   hSetBuffering stdout $ BlockBuffering Nothing
   hSetBinaryMode stdout True
-  bracket (socketPair AF_UNIX Datagram defaultProtocol) (\(soc1, soc2) -> close soc1 *> close soc2) $ \(soc1, soc2) -> do
-    let runMpvIPC = withFdSocket soc2 $ \fd -> do
-          let process =
-                setStdin nullStream . setStdout nullStream . setStderr nullStream $
-                  shell ("mpv --input-ipc-client=fd://" <> show fd <> " --idle --keep-open --profile=youtube-mpv")
-          withProcessWait process $ \p ->
-            let parseAndSendCommand l = case runGetOrFail get l of
-                  Right (uncomsumed, b, NativeMessage cmd) -> do
-                    hEncodeAndSend . MPVInfo $ "Comsumed " <> T.pack (show b) <> " bytes."
-                    hEncodeAndSend cmd
-                    sendAll soc1 cmd
-                    sendAll soc1 "\n"
-                    parseAndSendCommand uncomsumed
-                  Left _ -> pure ()
-                commandLoop = BL.getContents >>= parseAndSendCommand
-             in (MPVError "commadLoop ended before mpv" <$ commandLoop) `race` waitExitCode p
-        mpvOutputPrintloop = SOC.getContents soc1 >>= traverse_ ((*> hFlush stdout) . hEncodeAndSend) . BL.lines
-    try ((MPVError "mpvOutputPrintloop ended before mpv" <$ mpvOutputPrintloop) `race` runMpvIPC) >>= \case
-      Left (err :: SomeException) -> hEncodeAndSend . MPVError . T.pack $ show err
-      Right (Right (Right ExitSuccess)) -> hEncodeAndSend $ MPVInfo "Done, all good"
-      Right (Right (Right oops)) -> hEncodeAndSend . MPVError . T.pack $ show oops
-      Right (Right (Left oops)) -> hEncodeAndSend oops
-      Right (Left err) -> hEncodeAndSend err
+  findExecutable [osstr|mpv|] >>= \case
+    Nothing -> do
+      hEncodeAndSend $ MPVError "mpv not found"
+      exitWith $ ExitFailure 127
+    Just mpv -> do
+      mpv' <- FP.decodeUtf mpv
+      bracket (socketPair AF_UNIX Datagram defaultProtocol) (\(soc1, soc2) -> close soc1 *> close soc2) $ \(soc1, soc2) -> do
+        let runMpvIPC = withFdSocket soc2 $ \fd -> do
+              let process =
+                    setStdin nullStream . setStdout nullStream . setStderr nullStream $
+                      proc
+                        mpv'
+                        [ "--input-ipc-client=fd://" <> show fd,
+                          "--idle",
+                          "--keep-open",
+                          "--profile=youtube-mpv"
+                        ]
+              withProcessTerm process $ \p ->
+                let parseAndSendCommand l = case runGetOrFail get l of
+                      Right (uncomsumed, b, NativeMessage cmd) -> do
+                        hEncodeAndSend . MPVInfo $ "Comsumed " <> T.pack (show b) <> " bytes."
+                        hEncodeAndSend cmd
+                        sendAll soc1 cmd
+                        sendAll soc1 "\n"
+                        parseAndSendCommand uncomsumed
+                      Left _ -> pure ()
+                    commandLoop = BL.getContents >>= parseAndSendCommand
+                 in (MPVError "commadLoop ended before mpv" <$ commandLoop) `race` waitExitCode p
+            mpvOutputPrintloop = SOC.getContents soc1 >>= traverse_ ((*> hFlush stdout) . hEncodeAndSend) . BL.lines
+        try ((MPVError "mpvOutputPrintloop ended before mpv" <$ mpvOutputPrintloop) `race` runMpvIPC) >>= \case
+          Left (err :: SomeException) -> hEncodeAndSend . MPVError . T.pack $ show err
+          Right (Right (Right ExitSuccess)) -> hEncodeAndSend $ MPVInfo "Done, all good"
+          Right (Right (Right oops)) -> hEncodeAndSend . MPVError . T.pack $ show oops
+          Right (Right (Left oops)) -> hEncodeAndSend oops
+          Right (Left err) -> hEncodeAndSend err
